e4a2f9bf9612197f368ea5746d74c0f99e6c15ce
pbalazs89 <Gotenkslol1>
2019-10-22 19:02:08 +0200

    Robostart first commit

---
+++ src/robots.js
@@ -0,0 +1,24 @@
+export const robots = [
+
+{
+    id:1,
+    name: 'Leanne Graham',
+    username: 'Bret',
+    email: 'sincere@april.biz'
+},
+
+{
+    id:2,
+    name: 'Leanne Graham2',
+    username: 'Bret2',
+    email: 'sincere@april.biz2'
+},
+
+{
+    id:3,
+    name: 'Leanne Graham3',
+    username: 'Bret3',
+    email: 'sincere@april.biz3'
+},
+
+]
